0a9b8a667232f20b7eb7f33abba544808e64c847
Abdullah Ibn Shahin <arbaazmir@icloud.com>
2024-05-07 17:15:59 +0400

    vercel url updated

--- src/components/ProfileContainerComp.vue
+++ src/components/ProfileContainerComp.vue
@@ -35,7 +35,7 @@ const user = computed(() => store.getProfile)
 const linkStore = useLinksStore()
 const editProfileShow = ref(false)
 const showLogout = ref(false)
-const link = 'https://vue-pinpoint.onrender.com/shared/' + user.username
+const link = 'https://vue-pinpoint.vercel.app/shared/' + user.username
 const logout = async () => {
   store.setUser(null)
   await linkStore.logoutSections()
